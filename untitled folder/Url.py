6df8486ffc8b9ed62a7683bbe00eb0e957dbfba5
ajayghela <ajay_ghela@hotmail.com>
2024-02-03 09:18:24 +0000

    second commit

--- untitled folder/Url.py
+++
@@ -1,6 +0,0 @@
-def trans_URL():
-    string = input("Enter the URL:")
-    URL = string.lower().replace(" ", '-')
-    print(URL)
-
-trans_URL()
--- untitledfolder/Url.py
+++
@@ -1,7 +0,0 @@
-def trans_URL():
-    string = input("Enter the URL:")
-    URL = string.lower().replace(" ", '-')
-    return URL
-
-modified_URL = trans_URL()
-print(modified_URL)
