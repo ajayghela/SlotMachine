4ad1fb8e1555ee7dea430d4541fd385aea3eb798
ajayghela <ajay_ghela@hotmail.com>
2024-02-11 13:05:27 +0000

    Transposition

--- Slot Machine/Main.py
+++ Slot Machine/Main.py
@@ -37,9 +37,11 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row], "|")
+                print(column[row], end= " | ")
             else:
-                print(column[row])
+                print(column[row], end="")
+                
+        print()
                 
 
 def deposit():
@@ -94,5 +96,8 @@ def main():
         else:
             break
     print(f"You are betting £{bet} on {lines} lines.  Total bet is equal to: £{total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
