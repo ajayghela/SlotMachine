55973b95d880176df934673959d106646d5168d6
ajayghela <ajay_ghela@hotmail.com>
2024-03-05 15:39:53 +0000

    5/3/2024 update def spin() added

--- Slot Machine/Main.py
+++ Slot Machine/Main.py
@@ -14,6 +14,30 @@ symbol_count ={
     "D" : 5
 }
 
+symbol_value ={
+    "A" : 5,
+    "B" : 4,
+    "C" : 3,
+    "D" : 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
+
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols =[]
     for symbol, symbol_count in symbols.items():
@@ -83,9 +107,7 @@ def get_bet():
             print("please enter a number.")
     return bet 
 
-
-def main():
-    balance = deposit()
+def spin():
     lines = get_number_of_line()
     while True:
         bet = get_bet()
@@ -99,5 +121,20 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f'You won £{winnings}.')
+    print(f'You won on lines:', *winning_lines )
+    return winning_lines - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        spin = input("Press enter to spin (q to quit).")
+        if spin == 'q':
+            break
+        balance += spin()
+    
 
 main()
